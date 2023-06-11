317a76ce17db29c3d9b8061e2a6f030239a9f92a
ZombiMigz <chunyumikey@gmail.com>
2023-06-11 16:07:45 -0700

    adds mui

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { ThemeOptions } from "@mui/material/styles";
+import { ThemeProvider } from "@emotion/react";
+
+export const themeOptions: ThemeOptions = {
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#00843d",
+    },
+    secondary: {
+      main: "#ffb500",
+    },
+  },
+  typography: {
+    fontFamily: '"Helvetica", "Arial", sans-serif',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={themeOptions}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
